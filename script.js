337831114627030ab29d947b414dde61003bfc38
akshayjith <137667911+akshayjith4@users.noreply.github.com>
2025-02-26 11:57:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ document.addEventListener("keydown", function (event) {
 });
 
 document.addEventListener("keydown", function (event) {
-    if (event.ctrlKey && event.key.toLowerCase() === "p") {
+    if (event.ctrlKey && event.key.toLowerCase() === "f") {
         event.preventDefault(); // Prevents the print dialog
         window.location.href = "https://flappybird.io/"; // Redirects to Flappy Bird
     }
